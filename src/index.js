0d456a4e17f56cb37df160d452ad220296335b2c
hyunjiq <edalnim12@gmail.com>
2024-08-23 15:51:36 +0900

    프로젝트 초기 설정: Boilerplate 코드 추가 및 컴포넌트 구조 정리

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+//부트스트랩, 아이콘, 스와이퍼
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'swiper/swiper-bundle.css';
+import 'swiper/css'; 
+
+// font, scss
+import './fonts/fonts.css';
+import './scss/common.scss';
+import './scss/subway.scss';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+        <App/>
+  </BrowserRouter>
+);
