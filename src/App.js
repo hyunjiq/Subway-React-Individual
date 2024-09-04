4338d03ec3021f88709ba64cbffe8f41bb042474
hyunjiq <edalnim12@gmail.com>
2024-09-04 17:49:30 +0900

    퀵메뉴 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Ft from "./component/Ft";
 import Hd from "./component/Hd";
 import Maincontents from "./component/Maincontents";
+import Quik from "./component/Quik";
 
 function App() {
   return (
     <>
       <Hd></Hd>
       <Maincontents></Maincontents>
+      <Quik></Quik>
       <Ft></Ft>
     </>
   );
---
+++ src/component/Quik.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+import { Styleicon } from "./style"
+
+
+function Quik() {
+    return (
+        <aside className="position-fixed">
+            <Link className="findbtn">
+                <span className="">매장찾기</span>
+            </Link>
+            <a href="#" className="topbtn">
+                <Styleicon content="\F145">                    
+                </Styleicon>
+                <span className="visually-hidden">위로가기</span>
+            </a>
+        </aside>
+    )
+}
+
+export default Quik
