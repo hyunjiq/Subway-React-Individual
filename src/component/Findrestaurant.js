7c66dd5c7c2d4fe19fa9cbcaaf5050c530a1377a
hyunjiq <edalnim12@gmail.com>
2024-09-10 16:14:01 +0900

    카카오지도에 사용할 위도경도 json추가,SVG 추가, 매장찾기 팝업 추가

--- src/component/Findrestaurant.js
+++ src/component/Findrestaurant.js
@@ -1,33 +1,59 @@
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import data from '../data/db.json';
 
 function FindRestaurant() {
+  const [mapCenter, setMapCenter] = useState({
+    let: 37.5012056,
+    lon: 126.8827953
+  });
+
   useEffect(() => {
-   
     const script = document.createElement('script');
     script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=f281f3543f5e84d03f2d0bed36be1a3b&autoload=false`;
     script.async = true;
     document.head.appendChild(script);
 
     script.onload = () => {
-      
       window.kakao.maps.load(() => {
-        const mapContainer = document.querySelector('.findmap');
-        const mapOption = { 
-          center: new window.kakao.maps.LatLng(37.5012056, 126.8827953), 
-          level: 3 
-        };
-        const map = new window.kakao.maps.Map(mapContainer, mapOption); 
+        try {
+          const mapContainer = document.querySelector('.findmap');
+          if (!mapContainer) {
+            console.error('Map container not found.');
+            return;
+          }
 
-       
-        const markerPosition = new window.kakao.maps.LatLng(37.5665, 126.9780); 
-        const marker = new window.kakao.maps.Marker({
-          position: markerPosition
-        });
-        marker.setMap(map);
+          const mapOption = { 
+            center: new window.kakao.maps.LatLng(mapCenter.let, mapCenter.lon), 
+            level: 3 
+          };
+          const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+          const markerPosition = new window.kakao.maps.LatLng(mapCenter.let, mapCenter.lon); 
+          const marker = new window.kakao.maps.Marker({
+            position: markerPosition
+          });
+          marker.setMap(map);
+
+          
+          const updateMapCenter = () => {
+            const newCenter = new window.kakao.maps.LatLng(mapCenter.let, mapCenter.lon);
+            map.setCenter(newCenter);
+            marker.setPosition(newCenter);
+          };
+
+          updateMapCenter();
+
+        } catch (error) {
+          console.error('Error initializing Kakao map:', error);
+        }
       });
     };
-  }, []);
 
+    script.onerror = () => {
+      console.error('Failed to load the Kakao Maps script.');
+    };
+
+  }, [mapCenter]);  
   return (
     <div className='findrestaurant'>
       <div className='findbanner position-fixed'>
@@ -39,7 +65,30 @@ function FindRestaurant() {
         </div>        
       </div> 
       <div className='glass'></div>     
-      <div className="findmap"></div>
+      <div className="findmap position-relative">
+        <div className="findstore position-absolute">
+          <div className="mapcon">            
+            <ul>
+              {
+                data.submap.map((v, i) => (
+                  <li key={`submap${i}`}>
+                    <button 
+                      className='mapconli d-flex flex-column px-0' 
+                      onClick={() => {                        
+                        setMapCenter({ let: v.let, lon: v.lon }); 
+                      }}
+                    > 
+                      <span className='storenm'>{v.storenm}</span>
+                      <span className='adrs'>{v.adrs}</span>
+                      <span className='tel'>{v.tel}</span>
+                    </button>                    
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
